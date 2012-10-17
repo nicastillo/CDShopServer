a712d82939b4ec0fcd9f2775eaabb14ab5aa9def
Natalia Castillo <nich13@gmail.com>
2012-10-17 00:54:57 -0400

    DBAgent changes
    getProductCatalog now working

--- src/java/com/etechies/server/dbagent/DBAgent.java
+++ src/java/com/etechies/server/dbagent/DBAgent.java
@@ -4,7 +4,7 @@
  */
 package com.etechies.server.dbagent;
 
-import com.etechies.server.beans.*;
+import com.etechies.server.dbagent.beans.Product;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
